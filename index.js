56ec66294fc37e8590091df520ae1f71c3d89df8
Alex Gabor <alex.gabor@mambu.com>
2020-01-30 17:46:05 +0200

    Update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const data = require('./data');
-const jsrsasign = require('./node_modules/jsrsasign');
+const jsrsasign = require('/node_modules/jsrsasign');
 
 module.exports = { data, jsrsasign };
